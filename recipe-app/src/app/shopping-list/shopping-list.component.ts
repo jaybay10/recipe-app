f9ffa98817e45814d94150816e059278d78647ae
Jason <68597004+jaybay10@users.noreply.github.com>
2023-05-17 19:17:00 +0200

    Added a recipe.modal.ts file as well as ingredient.modal.ts both these files act like a blueprint that can be called into their respective arrays.
    Recipe component and Shopping list component has additional html code, and did some styling for the shopping edit component, creating space between the buttons.

--- recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ingredient } from '../shared/ingriedent.modal';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+
+  ingredients: ingredient[] = [
+    new ingredient('Apples', 5),
+    new ingredient('Tomatoes', 10),
+    new ingredient('Bananas', 8)
+  ];
 
 }
