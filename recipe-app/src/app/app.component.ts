ab80b731bedfec34d37113a108097a9291efc9b1
Jason Bayford <bayfie10@gmail.com>
2023-05-22 18:46:14 +0200

    Naavigation between shopping-list and recipe has been built and works.
    Decoupling so of the html and tyescript file for recipe-list and recipe-item.

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+
+  recipe_feature: string = 'recipe';
+  shopping_list_feature: string = 'shopping-list';
+
+
+  onNavigate(feature: string){
+    this.recipe_feature = feature;
+    this.shopping_list_feature = feature;
+  }
 }
--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,4 @@ export class RecipeListComponent {
     new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRifQDyJ8OllHWJDWhKUDrNo39R-Qncq5gRIQ&usqp=CAU'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS16CPZLq9Jk-0GrYAaQ7kqSPS9lgspFYKo-A&usqp=CAU')
   ];
-
-
 }
