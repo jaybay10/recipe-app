33d913b8f55ea49c03ad41dd91f76d7c8731ae4e
Jason Bayford <bayfie10@gmail.com>
2023-05-23 14:12:31 +0200

    editted the the navigation, was a bit buggy. Did not seperate the two components accordingly.

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -9,11 +9,8 @@ export class AppComponent {
   title = 'recipe-app';
 
   recipe_feature: string = 'recipe';
-  shopping_list_feature: string = 'shopping-list';
-
 
   onNavigate(feature: string){
     this.recipe_feature = feature;
-    this.shopping_list_feature = feature;
   }
 }
